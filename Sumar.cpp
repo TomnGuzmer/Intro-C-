ae73c34834010d2daed6d765a7512e8177c8b23c
TomnGuzmer <31928247+TomnGuzmer@users.noreply.github.com>
2017-09-14 17:13:41 -0500

    Add files via upload

---
+++ Sumar.cpp
@@ -0,0 +1,22 @@
+#include <iostream> 
+
+int main()
+{
+int cout;
+int numero1; 
+int numero2; 
+int suma;
+ 
+std::cout << "Escriba el primer entero: "; 
+std::cin >> numero1; 
+ 
+std::cout << "Escriba el segundo entero: "; 
+std::cin >> numero2; 
+ 
+suma = numero1 + numero2; 
+ 
+std::cout << "La suma es " << suma << std::endl; 
+ 
+return 0; 
+ 
+}
